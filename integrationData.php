873843cef71e3b3ba8ca3f1ec53439bc1b8b577b
ec102188 <45929569+ec102188@users.noreply.github.com>
2018-12-17 19:36:33 -0800

    Add files via upload

---
+++ integrationData.php
@@ -0,0 +1,8 @@
+<?php
+
+// Integration data provided by Provider
+return [
+    'merchantId' => '{PROVIDER-MERCHANT-ID}',
+    'merchantKey' => '{PROVIDER-MERCHANT-KEY}',
+    'baseApiUrl' => '{PROVIDER-API-URL}',
+];
