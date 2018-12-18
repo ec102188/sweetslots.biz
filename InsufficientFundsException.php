70ed167cfb9bbe849a3168cebc9aa329e153e6ed
ec102188 <45929569+ec102188@users.noreply.github.com>
2018-12-17 19:37:53 -0800

    Add files via upload

---
+++ InsufficientFundsException.php
@@ -0,0 +1,10 @@
+<?php
+namespace providerBundle\exceptions;
+
+/**
+ * Class InsufficientFundsException
+ * @package providerBundle\exceptions
+ */
+class InsufficientFundsException extends \Exception
+{
+}
